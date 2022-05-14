631e6f1452e7153de9da02144748f2ff5b7df7aa
Desiders <maximzybin303serg08@mail.ru>
2022-05-14 20:23:00 +0300

    🐛 Fixed a bug with useless spaces

--- app/handlers/funny.py
+++ app/handlers/funny.py
@@ -6,73 +6,73 @@ from aiogram.types import Message
 STICKERS_FILE_IDS = [
     # KanekiKen
     "CAACAgQAAxkBAAEPsb1ieQXvAAHgo0bdEfJJ7CA-AAHKgdmBAAJPAQACgALNBQcDO7JDwJ\
-        vjJAQ",
+vjJAQ",
     "CAACAgQAAxkBAAEPsb9ieQYOk0tb0TfAx5irk_-eI9iqXAACSwEAAoACzQXHx8lgfd3\
-        RNyQE",
+RNyQE",
     # LINE_Cat_ear_girl_Necoco
     "CAACAgIAAxkBAAEPscNieQaatAqYD6QO3XjFAAFcXoP81LYAAo9oAALgo4IH_x-II5lUE\
-        dokBA",
+dokBA",
     "CAACAgIAAxkBAAEPscVieQbRTgvKNIDLweza27QJhKFRkAACpWgAAuCjggclFgRYTz0\
-        0xyQE",
+0xyQE",
     # LINE_Dong_Jin_Rice_hime
     "CAACAgIAAxkBAAEPscdieQcGOYEOlYn_QYG8YS-4XPFtlAACdjcAAuCjggfnHhRQKW1\
-        21CQE",
+21CQE",
     "CAACAgIAAxkBAAEPsclieQc6eP_j0KEZi4_cbstvuKYHOwACczcAAuCjggcBM4mGuZC\
-        criQE",
+criQE",
     # LINE_nachonekodayo
     "CAACAgIAAxkBAAEPsctieQdSQ8yjNmxGqgXgc08eDpewNQAC7WoAAuCjggefr1M1mI4\
-        DFCQE",
+DFCQE",
     "CAACAgIAAxkBAAEPsc1ieQdq2kAIwCb9kWmQG4M-uxOLhwAC-WoAAuCjggfjboP1BOs\
-        FECQE",
+FECQE",
     # SeiranElenoir
     "CAACAgQAAxkBAAEPsdFieQeWXMwg9LXqmh5z_45YDl7GDwACkwIAAqN9MRXdCfPXqqn\
-        JjyQE",
+JjyQE",
     # yamy_tyan_nyasticks
     "CAACAgIAAxkBAAEPsddieQfXtCGLze2VUQzmgX6vnF-BzQACSxAAAtvmwUoX1EPwOmu\
-        GFyQE",
+GFyQE",
     "CAACAgIAAxkBAAEPsdtieQf9NIQXHfOo2Ft7Ll5sorUzEAAC5Q4AApjWwUrJ7aDKQKi\
-        7yCQE",
+7yCQE",
     "CAACAgIAAxkBAAEPsd1ieQgXqlScTV6v99sd_c9iBwjRJwACFQ8AAoxnuUqhX6LFvID\
-        sZyQE",
+sZyQE",
     # LINE_Puu_Moody_Girl_4
     "CAACAgIAAxkBAAEPsd9ieQgqSfFx_yWW1nmaSv_Q8k37owAC5FkAAuCjgge-sw2IUzU\
-        8uSQE",
+8uSQE",
     "CAACAgIAAxkBAAEPseFieQhClqo8K8pWSUXO_Fo8igWvegAC7lkAAuCjggeBNrgK-Dm\
-        HkSQE",
+HkSQE",
     # Magic_Cat_Mori
     "CAACAgQAAxkBAAEPseNieQhoVFtGqqfqqLm81QQMwkxVvwACBAMAAifSHAfxjj_0cnN\
-        R2yQE",
+R2yQE",
     "CAACAgQAAxkBAAEPseVieQiEU_1jhsgVlSh_RJl6T-5HOAADAwACJ9IcB7sdrTNTL2\
-        OxJAQ",
+OxJAQ",
     # Flandrenice,
     "CAACAgIAAxkBAAEPsedieQilLFQ4Db917vuuI2QL0NKOEAACTgADfLufDoPlfvHf69\
-        lLJAQ",
+lLJAQ",
     # LINE_Girl_with_black_hair_TL
     "CAACAgIAAxkBAAEPselieQkAAXXRaT3wEugEYeA3jB3U-YUAAutjAALgo4IHgUlpWZWXo\
-        UwkBA",
+UwkBA",
     # LINE_CatGirl_Sticker_ENG
     "CAACAgIAAxkBAAEPsetieQk9ZdE98Eb7DDnvTQoghzk-GQACLzoAAuCjggfEE_UdZYK\
-        IniQE",
+IniQE",
     "CAACAgIAAxkBAAEPse1ieQlRiEOqiD-yvmWwm6DNhAABO9kAAkE6AALgo4IHDvGsE3ppF\
-        jIkBA",
+jIkBA",
     # LINE_Puni_never_smile
     "CAACAgIAAxkBAAEPse9ieQlhVB8tLC_znwUl88PItokN4AACyzMAAuCjggd8e9cLC7I\
-        2FyQE",
+2FyQE",
     "CAACAgIAAxkBAAEPsfFieQmb84SfOmeSiYo8JncAATgM4CkAAt0zAALgo4IHdSnFUI87D\
-        DIkBA",
+DIkBA",
     # LINE_Puniko_eat_everyday
     "CAACAgIAAxkBAAEPsfNieQmvQz025I6uFgjEPcdCAULbhwACViYAAuCjggcainPeqUt\
-        ZuyQE",
+ZuyQE",
     "CAACAgIAAxkBAAEPsfVieQnDJEdqFqByZvYT9K1cni9P_AACQCYAAuCjggfKnhdj9-b\
-        boiQE",
+boiQE",
     # LINE_Girls_Love_Story_2
     "CAACAgIAAxkBAAEPsfdieQnfhqcbiNSqAAGdn8XSohlaBxUAAmYmAALgo4IHHGLslzpuZ\
-        -kkBA",
+-kkBA",
     "CAACAgIAAxkBAAEPsflieQoSqKMhJJ2er57PYfnYTjPlQAACdCYAAuCjggc3zD9fRBW\
-        ICCQE",
+ICCQE",
     # LINE_MochiChan_V2
     "CAACAgIAAxkBAAEPsf1ieQpomhxPtLMR0y0giDCKumu9NgACnCUAAuCjggdqvf1Er4p\
-        WISQE",
+WISQE",
 ]
 
 
